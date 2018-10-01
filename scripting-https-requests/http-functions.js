aae341aa303e9fbfef866264a023102a28a71ed0
Bassem Kaddour <bassemkaddour@gmail.com>
2018-10-01 19:57:31 +0000

    step 5, exported as module

---
+++ scripting-https-requests/http-functions.js
@@ -0,0 +1,34 @@
+var https = require('https');
+
+function getHTML(options, callback) {
+  var outputString = '';
+
+  https.get(options, function (response) {
+
+    response.setEncoding('utf8');
+
+    response.on('data', function (data) {
+      outputString += data;
+    });
+
+
+    response.on('end', function () {
+      callback(outputString);
+    });
+
+  });
+}
+
+function printHTML (html) {
+  console.log(html);
+}
+
+var requestOptions = {
+  host: 'sytantris.github.io',
+  path: '/http-examples/step4.html'
+};
+
+module.exports = {
+  getHTML,
+  printHTML
+};
